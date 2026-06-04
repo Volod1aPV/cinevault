848061d2786126866a344280ac1e4abcb6fb134c
Volodymyr Panovyk <panovvo@student.uzlabina.cz>
2026-06-04 16:13:27 +0200

    config: allow mobile IP origin in development configs for HMR

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  allowedDevOrigins: ['10.84.197.6', '10.84.197.6:3000']
 };
 
 export default nextConfig;
